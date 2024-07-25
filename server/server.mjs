524de8b6304b5d43fc87e8489e10123680a83a3d
Felix Viola <felix.viola@uni-bonn.de>
2024-07-25 15:40:49 +0200

    serve static folder

--- server/server.mjs
+++ server/server.mjs
@@ -3,9 +3,7 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 80;
 
-app.get('/', (req, res) => {
-  res.send('Hello, Docker!');
-});
+app.use(express.static('static'));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
