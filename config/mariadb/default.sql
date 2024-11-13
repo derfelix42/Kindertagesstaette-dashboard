bf164d1a9d1c5b5d67743912bf0c56867059f6ec
RedAICM <45194973+RedAicm@users.noreply.github.com>
2024-11-13 15:39:14 +0100

    Adapt sql scheme

--- config/mariadb/default.sql
+++ config/mariadb/default.sql
@@ -1,9 +1,11 @@
 CREATE TABLE `quotes` (
   `id` int NOT NULL AUTO_INCREMENT,
   `quote` text NOT NULL,
-  `author` varchar(255),
+  `author` varchar(255) NOT NULL DEFAULT "Anonymous",
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `recipient` varchar(255),
+  `emotion` varchar(255),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `quotes` (`quote`, `author`) VALUES ('The only way to get rid of a temptation is to yield to it.', 'Oscar Wilde');
+LOAD DATA LOCAL INFILE '/data/quotes.csv' INTO TABLE `quotes` FIELDS TERMINATED BY ';' (`quote`, `author`, `recipient`, `emotion`) SET `recipient`= NULLIF(`recipient`, ''), `emotion`= NULLIF(`emotion`, ''),`author` = IFNULL(NULLIF(`author`, ''), 'Anonymous');
